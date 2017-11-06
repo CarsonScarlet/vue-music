af1072dc35f9bde79f17b7770f41d2b51557bbc7
CarsonScarlet <gaoocx@163.com>
2017-11-06 14:25:44 +0800

    初始化版本

---
+++ src/common/js/dom.js
@@ -0,0 +1,56 @@
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
+
+export function addClass(el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+
+export function getData(el, name, val) {
+  const prefix = 'data-'
+  if (val) {
+    return el.setAttribute(prefix + name, val)
+  }
+  return el.getAttribute(prefix + name)
+}
+
+// 对transform的封装
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    // 如果有的话就挨个遍历
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  // 如果没有那就是浏览器有问题,返回false
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+
+  if (vendor === 'standard') {
+    return style
+  }
+  // vender + 首字母大写 + 剩余的部分
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
